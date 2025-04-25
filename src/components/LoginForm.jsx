bb414d8594437889938f3775c2856626e1b4035c
ahmedrochdi <ahmedrochdi824@gmail.com>
2025-04-25 13:37:04 +0100

    useState

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -8,6 +8,7 @@ export function LoginForm() {
     <form onSubmit= {(e) => {
         e.preventDefault(); // Empêche la page de se recharger
         const formData = new FormData(e.target);//(e.target) ici cible le form lui-méme 
+        // new FormData() : Crée un objet qui récupère automatiquement toutes les données du formulaire.
         const username = formData.get("username")
         const password = formData.get("password")
         
@@ -26,20 +27,28 @@ export function LoginForm() {
         }}>Username:</label>
         <br />
         <br />
-<input id='username' value={username}
- onChange={(e) => { setUsername(e.target.value);
-     console.log(e.target.value ) ;
- 
-}}  name="username" style={{padding:"10px", borderRadius:"6px", 
-boxShadow:"initial",width:"300px"}}/>
+<input
+    id='username'
+    value={username}
+    onChange={(e) => { setUsername(e.target.value);
+    console.log(e.target.value ) ;}} 
+    name="username"
+    style={{padding:"10px", borderRadius:"6px", boxShadow:"initial",width:"300px"
+
+ }}/>
 <br />
 <br />
-<label htmlFor="Email" style={{cursor:"Pointer",
-            fontSize:"32px"
-        }}>Your Email:</label>
-        <br />
-        <br />
-<input id='Email' name="Email" type="email" style={{padding:"10px", borderRadius:"6px", boxShadow:"initial", width:"300px"}}/>
+<label htmlFor="Email" 
+style={{cursor:"Pointer",
+        fontSize:"32px"
+        }}>Your Email:
+</label>
+<br />
+<br />
+<input id='Email'
+ name="Email" 
+ type="email" 
+ style={{padding:"10px", borderRadius:"6px", boxShadow:"initial", width:"300px"}}/>
 <br />
 <br />
 <label htmlFor="Pasword" style={{cursor:"Pointer",
@@ -47,14 +56,19 @@ boxShadow:"initial",width:"300px"}}/>
         }} >Password:</label>
         <br />
         <br />
-<input id='Password' name="password" type="password"
- onChange={(e)=> {console.log("lettre:",e.target.value);}} 
-style={{padding:"10px", borderRadius:"6px", boxShadow:"initial", width:"300px"}}/>
+<input
+    id='Password' 
+    name="password" 
+    type="password"
+    onChange={(e)=> {console.log("lettre:",e.target.value);}} 
+    style={{padding:"10px", borderRadius:"6px", boxShadow:"initial", width:"300px"}}/>
 <br />
 <br />
-<button  onClick={(e) =>{console.log( "buttoncliquer")} }
+<button  
+onClick={(e) =>{console.log( "buttoncliquer")} }
 style={{display:"block", padding:"10px",width:"100px",   fontWeight: "bold" , fontSize:"20px", cursor:"pointer"}}>
-    Login </button>
+    Login
+</button>
 
     </form>
 );
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,20 +1,38 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export function RegisterForm(){
-    const [username , setUsername]=useState("");
-    const [password , setPassword]=useState("");
-    const [displayName , setDisplayName] = useState("");
-    return <form>
+    // const [username , setUsername]=useState("");
+    // const [password , setPassword]=useState("");
+    // const [displayName , setDisplayName]=useState("");
+    
+    // const isDisabled =  !username || !password || !displayName; // Désactivé si un champ est vide
+
+    //Gestion des champs de formulaire avec un seul state (formFields)
+    const [formFields ,setFormFields]=useState({
+        username:'',
+        password:'',
+        displayName:''
+    });
+    console.log(formFields);
+    return <form onSubmit={(e)=>{ e.preventDefault();}}> 
         <div>
             <label htmlFor="username"> Username:</label>
             <br />
             <br />
         <input 
         id="username"
-        value={username}
-        onChange={(e)=> { setUsername (e.target.value);
-
-          }} />
+        value={formFields.username} // important for binding with state
+       // onChange={(e)=> { setFormFields (e.target.value);// also this important for binding  }} 
+       onChange={
+        (e)=>{
+         setFormFields((currentState) => 
+        ({ ...currentState , // React te donne le VRAI état actuel
+            username: e.target.value,
+        }))
+        }
+        }
+         
+         />
         
         </div>
          <br />
@@ -22,11 +40,15 @@ export function RegisterForm(){
             <label htmlFor="password" >Password : </label>
             <br />
             <br />
-        <input id="password" 
-        value={password}
-        onChange={(e)=> { setPassword (e.target.value);
-
-        }} />
+        <input 
+        id="password" 
+        value={formFields.password}
+        //onChange={(e)=> { setPassword (e.target.value);}}
+        onChange={(e)=>{ setFormFields((currentState) => 
+            ({ ...currentState , // React te donne le VRAI état actuel
+                password: e.target.value,
+            }))}}
+          />
         
         </div>
          <br />
@@ -34,12 +56,53 @@ export function RegisterForm(){
             <label htmlFor="displayName"> Display name : </label> 
             <br />
             <br />
-        <input id="displayName"  
-        value={displayName}
-        onChange={(e)=> { setDisplayName (e.target.value);
-
-        }} />
+        <input 
+        id="displayName"  
+        value={formFields.displayName}
+       // onChange={(e)=> { setDisplayName (e.target.value);}}
+       onChange={(e)=>{
+        setFormFields((currentState) => 
+            ({ ...currentState , // React te donne le VRAI état actuel
+                displayName: e.target.value,
+            }))
+       }}
+        />
         
         </div>
+        <br />
+        <div>
+            <span>
+                username: {formFields.username}
+            </span>
+            <br />
+            <br />
+            <span> password : <span></span>
+                 
+                 <span style={{
+                    color: 'transparent',
+                    textShadow: '0 0 5px rgba(0, 0, 0, 0.5)',
+                    fontFamily: 'monospace'
+                    }}>
+                    {formFields.password}
+                </span>  
+            </span>
+            <br />
+            <br />
+            <span> display name : {formFields.displayName} </span>
+           
+            
+        </div>
+        <br />
+        {
+        /* <button   
+         disabled={isDisabled} 
+       style={{
+        backgroundColor: isDisabled ? "white" : "green",
+        padding: "10px",
+        cursor: isDisabled ? "not-allowed" : "pointer"
+      }}
+      >Sing Up
+      </button> */
+      }
     </form>
 }
